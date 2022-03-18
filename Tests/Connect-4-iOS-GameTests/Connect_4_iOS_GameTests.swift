f6db5f8196ff00212276422f36667f3b06c1cef9
Ricky Chuang <songsongyyohya@gmail.com>
2022-03-19 02:58:58 +0800

    Create Connect_4_iOS_GameTests.swift

---
+++ Tests/Connect-4-iOS-GameTests/Connect_4_iOS_GameTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Connect_4_iOS_Game
+
+final class Connect_4_iOS_GameTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Connect_4_iOS_Game().text, "Hello, World!")
+    }
+}
