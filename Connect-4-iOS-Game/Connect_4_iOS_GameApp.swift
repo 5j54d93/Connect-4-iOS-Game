55f82e1939ef581ead9ed176e9e260293f5aec20
Ricky Chuang <songsongyyohya@gmail.com>
2022-03-18 18:21:27 +0800

    Initial Commit

---
+++ Connect-4-iOS-Game/Connect_4_iOS_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Connect_4_iOS_GameApp.swift
+//  Connect-4-iOS-Game
+//
+//  Created by 莊智凱 on 2022/3/18.
+//
+
+import SwiftUI
+
+@main
+struct Connect_4_iOS_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
